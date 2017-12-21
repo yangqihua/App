b33770c322b3e63beea875dd16f61c69b20b4e70
yangqihua <904693433@qq.com>
2017-12-21 20:31:08 +0800

    优化

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -17,7 +17,6 @@ import PhotoPage from '../pages/common/PhotoPage';
 import VideoPlayerTest from '../test/VideoPlayerTest'
 import VideoTest from '../test/VideoTest'
 import CacheImage from '../test/CacheImage'
-import DropDownTest from '../test/DropDownTest'
 const StackOptions = ({navigation}) => {
 	let {goBack,headerRight} = navigation;
 	const headerLeft = (
@@ -58,10 +57,6 @@ const AppNavigation = StackNavigator(
 		},
 
 		// test page
-		DropDownTest: {
-			screen: DropDownTest,
-		},
-
 		VideoPlayerTest: {
 			screen: VideoPlayerTest,
 		},
